2580b2ded26109c25d07cddf2ef2c301e2b4fd29
Mathieu Benoit <mathben@technolibre.ca>
2020-09-23 14:59:38 -0400

    [MIG] res_partner_attributes_add_SIN: Migration to 12.0

--- res_partner_attributes_add_SIN/__manifest__.py
+++ res_partner_attributes_add_SIN/__manifest__.py
@@ -1,10 +1,9 @@
-# -*- coding: utf-8 -*-
 # © 2016 Savoir-faire Linux
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
     'name': 'Canada Social Insurance Number (SIN/NAS)',
-    'version': '10.0.1.0.0',
+    'version': "12.0.1.0.0",
     'author': "Savoir-faire Linux,Odoo Community Association (OCA)",
     'website': 'http://www.savoirfairelinux.com',
     'license': 'AGPL-3',
